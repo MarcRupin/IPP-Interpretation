3bf502f0cbdcecbd73483e4231e7ca559866e531
MarcRupin <78181771+MarcRupin@users.noreply.github.com>
2021-06-17 12:38:15 +0800

    Update IPP.py

--- IPP.py
+++ IPP.py
@@ -7,13 +7,7 @@ def main():
     num5 = float(input("Input the number of respondents for Very Often"))
     weighted_num = ((num1 * 1) + (num2 * 2) + (num3 * 3) + (num4 * 4) + (num5 * 5))
     mean = weighted_num / (num1 + num2 + num3 + num4 + num5)
-    var1 = (((((5 - mean) ** 2) * num5) + (((4 - mean) ** 2) * num4) + (((3 - mean) ** 2) * num3) + (((2 - mean) ** 2)
-                                                                                                     * num2) + (((1 -
-                                                                                                                  mean
-                                                                                                                  ) ** 2
-                                                                                                                 )
-                                                                                                                * num1))
-            / ((num1 + num2 + num3 + num4 + num5) - 1))
+    var1 = (((((5 - mean) ** 2) * num5) + (((4 - mean) ** 2) * num4) + (((3 - mean) ** 2) * num3) + (((2 - mean) ** 2) * num2) + (((1 - mean) ** 2)* num1)) / ((num1 + num2 + num3 + num4 + num5) - 1))
     stddev = var1 ** 0.5
     print(mean)
     print(var1)
